109c16dc92de1a6f9ee90083422fb7fa8a13402b
YMTSTK <87373329+YMTSTK@users.noreply.github.com>
2022-11-19 15:19:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
+from sklearn import preprocessing
+
+model = pickle.load(open('diabetics.pkl','rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/diabetic',methods=['POST'])
+def diabetic():
+
+    Pregnancies=request.form.get('Pregnancies')
+    glucose=request.form.get('glucose')
+    bp=request.form.get('bp')
+    skin=request.form.get('skin')
+    insulin=request.form.get('insulin')
+    bmi=request.form.get('bmi')
+    pedigree=request.form.get('pedigree')
+    age=request.form.get('age')
+
+    list = [Pregnancies,glucose,bp,skin,insulin,bmi,pedigree,age]
+
+    #print(list)
+    #from sklearn.preprocessing import LabelEncoder
+    #le = LabelEncoder()
+   # li = le.fit_transform(list)
+
+    result = model.predict([list])[0]
+
+    return jsonify({'disease': str(result)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
